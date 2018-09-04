83a92badf2799627450a8c944a1947d055abc3f4
Negassa Berhanu <negassab16@gmail.com>
2018-09-04 23:07:27 +0300

    Chapter 22 commit -> Completed chapter 22, using Intent, File, Uri and Environment coded the CaptureFragment class and tested it. It works, Woohoogit status There is a bug, when changing orientation after taking an image it crashes saying trying to use recycled bitmap, fix it. Re-read inorder to fully understand what you coded.

--- app/src/main/java/com/negassagisila/whereitssnap/CaptureFragment.java
+++ app/src/main/java/com/negassagisila/whereitssnap/CaptureFragment.java
@@ -1,18 +1,42 @@
 package com.negassagisila.whereitssnap;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Negassa Berhanu on 9/2/18.
  */
 
 public class CaptureFragment extends Fragment {
+    private final static int CAMERA_REQUEST = 123;
+    private ImageView mImageView;
+    //the file path for the photo
+    String mCurrentPhotoPath;
+    //where the captured image is stored
+    private Uri mImageUri = Uri.EMPTY;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +46,82 @@ public class CaptureFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_capture, container, false);
+        final View view = inflater.inflate(R.layout.fragment_capture, container, false);
 
-        /*View view = inflater.inflate(R.layout.fragment_capture, container, false);
-        return view;*/
+        mImageView = view.findViewById(R.id.imageView);
+        Button btnCapture = view.findViewById(R.id.btnCapture);
+        Button btnSave = view.findViewById(R.id.btnSave);
+
+        final EditText mEditTextTitle = view.findViewById(R.id.editTextTitle);
+        final EditText mEditTextTag1 = view.findViewById(R.id.editTextTag1);
+        final EditText mEditTextTag2 = view.findViewById(R.id.editTextTag2);
+        final EditText mEditTextTag3 = view.findViewById(R.id.editTextTag3);
+
+        //listen for clicks on the capture button
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File photoFile = null;
+                try{
+                    photoFile = createImageFile();
+                } catch (IOException iex) {
+                    //error occured while creating the File
+                    Log.e("error", "error creating file");
+                }
+                //continue only if the File was successfully created
+                if (photoFile != null) {
+                    mImageUri = Uri.fromFile(photoFile);
+                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Not yet, give it a little bit of time", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+    private File createImageFile() throws IOException {
+        //create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "__";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, //filename
+                ".jpg", //extension
+                storageDir //folder
+                );
+
+        //save for use with ACTION_VIEW intent
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            try {
+                mImageView.setImageURI(Uri.parse(mImageUri.toString()));
+            } catch (Exception e) {
+                Log.e("Error", "Uri not set");
+            }
+        } else {
+            mImageUri = Uri.EMPTY;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //make sure we don't run out of memory
+        BitmapDrawable bd = (BitmapDrawable) mImageView.getDrawable();
+        bd.getBitmap().recycle();
+        mImageView.setImageBitmap(null);
     }
 }
