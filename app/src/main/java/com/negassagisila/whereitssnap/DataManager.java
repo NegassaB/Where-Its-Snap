95334dd15ffdb4f7b381cb55bd5c7b1db4e2bee1
Negassa Berhanu <negassab16@gmail.com>
2018-09-26 15:57:05 +0300

    done with chapter 26. Added the maps connecting intent. nearly complete.

--- app/src/main/java/com/negassagisila/whereitssnap/DataManager.java
+++ app/src/main/java/com/negassagisila/whereitssnap/DataManager.java
@@ -20,10 +20,14 @@ public class DataManager {
     public static final String TABLE_ROW_TITLE = "image_title";
     public static final String TABLE_ROW_URI = "image_uri";
 
+    //new with version 2
+    public static final String TABLE_ROW_LOCATION_LAT = "gps_location_lat";
+    public static final String TABLE_ROW_LOCATION_LONG = "gps_location_long";
+
     /*Next we have a private static final strings for each row/table
     * that we need to refer to just inside this class*/
     private static final String DB_NAME = "wis_db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
     private static final String TABLE_PHOTOS = "wis_table_photos";
     private static final String TABLE_TAGS = "wis_table_tags";
     private static final String TABLE_ROW_TAG1 = "tag1";
@@ -44,9 +48,12 @@ public class DataManager {
         //add all the details to the photos table
         String query = "INSERT INTO " + TABLE_PHOTOS + " (" +
                 TABLE_ROW_TITLE + ", " + TABLE_ROW_URI + ", " +
+                TABLE_ROW_LOCATION_LAT + ", " + TABLE_ROW_LOCATION_LONG + ", " +
                 TABLE_ROW_TAG1 + ", " + TABLE_ROW_TAG2 + ", " +
                 TABLE_ROW_TAG3 + ") VALUES (" + "'" + photo.getTitle() +
                 "'" + ", " + "'" + photo.getmStorageLocation() + "'" + ", " +
+                photo.getGpsLocation().getLatitude() + ", " +
+                photo.getGpsLocation().getLongitude() + ", " +
                 "'" + photo.getTag1() + "'" + ", " +
                 "'" + photo.getTag2() + "'" + ", " +
                 "'" + photo.getTag3() + "'" + ");";
@@ -138,6 +145,8 @@ public class DataManager {
                     " integer primary key autoincrement not null," +
                     TABLE_ROW_TITLE + " text not null," +
                     TABLE_ROW_URI + " text not null," +
+                    TABLE_ROW_LOCATION_LAT + " real," +
+                    TABLE_ROW_LOCATION_LONG + " real," +
                     TABLE_ROW_TAG1 + " text not null," +
                     TABLE_ROW_TAG2 + " text not null," +
                     TABLE_ROW_TAG3 + " text not null" +
@@ -157,7 +166,14 @@ public class DataManager {
         //this method only runs when we increment DB_VERSION
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            //update for version 2
+            String addLongColumn = "ALTER TABLE " + TABLE_PHOTOS + " ADD " +
+                    TABLE_ROW_LOCATION_LONG + " real;";
+            db.execSQL(addLongColumn);
 
+            String addLatColumn = "ALTER TABLE " + TABLE_PHOTOS + " ADD " +
+                    TABLE_ROW_LOCATION_LAT + " real;";
+            db.execSQL(addLatColumn);
         }
     }
 }
